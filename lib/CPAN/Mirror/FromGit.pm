7819e3881bbe30f428713013c8ab6a8d274f0b85
Eden Cardim <edencardim@gmail.com>
2013-12-15 00:47:51 -0200

    index builder functional

--- lib/CPAN/Mirror/FromGit.pm
+++ lib/CPAN/Mirror/FromGit.pm
@@ -1,19 +1,67 @@
 package CPAN::Mirror::FromGit;
+
+# ABSTRACT: Use git repos instead of cpan distributions
+
 use Moose;
 use Path::Class ();
 use File::Temp;
 use Cwd;
 use PPI;
+use File::HomeDir;
+use YAML ();
 
-# ABSTRACT: Use git repos instead of cpan distributions
+with 'MooseX::ConfigFromFile';
+
+sub _get_default_config_file {
+  my $home = File::HomeDir->my_home;
+  my $file = qq{$home/.cpangit};
+  open( my ($fh), '>', $file ) || die qq{Couldn't open $file: $!}
+    unless -f $file;
+  return $file;
+}
+
+sub get_config_from_file {
+  my ( $class, $file ) = @_;
+  return YAML::LoadFile($file);
+}
+
+has repos => ( isa => 'ArrayRef', is => 'ro', required => 1 );
+
+has home_dir => ( isa => 'Str', is => 'ro', required => 1, lazy_build => 1 );
+
+has module_index_file =>
+  ( isa => 'Str', is => 'ro', required => 1, lazy_build => 1 );
+
+has module_index =>
+  ( isa => 'HashRef', is => 'ro', required => 1, lazy_build => 1 );
+
+sub _build_home_dir {
+  my $home = File::HomeDir->my_home;
+  my $dir  = "$home/.cpangit";
+  mkdir($dir) unless -e $dir;
+  return $dir;
+}
+
+sub _build_module_index_file {
+  my $index = shift->home_dir . '/index.yaml';
+  open( my ($fh), '>', $index ) || die "Couldn't open $index: $!"
+    unless -e $index;
+  return $index;
+}
+
+sub _build_module_index {
+  my ($self) = @_;
+  return YAML::LoadFile( $self->module_index_file );
+}
 
 sub checkout {
-  my($self, $repo) = @_;
+  my ( $self, $repo ) = @_;
   my $curdir = Cwd::getcwd();
-  my $tmpdir = File::Temp::tempdir();
+  my $tmpdir = File::Temp::tempdir( CLEANUP => 1 );
   chdir($tmpdir);
-  my $ts = time();
-  system(qq{git clone $repo $ts});
+  my $ts  = time();
+  my $cmd = qq{git clone $repo $ts};
+  system($cmd);
   chdir($curdir);
   return $tmpdir;
 }
@@ -35,8 +83,33 @@ sub scan_repo {
       push @packages, map { $_->namespace } @$local_pkgs if $local_pkgs;
     }
   );
+
   # TODO identify versions
-  return map { $_ => undef } @packages;
+  return map { $_ => undef } grep {length} @packages;
+}
+
+sub get_repo_list {
+  my ($self) = @_;
+  my @repos = @{ $self->repos };
+  chomp for @repos;
+  return grep { defined && length } @repos;
+}
+
+sub build_repo_index {
+  my ($self) = @_;
+  my @repos = $self->get_repo_list;
+  my %index;
+  for my $repo (@repos) {
+    my %pkgs = $self->scan_repo($repo);
+    $index{$_} = $repo for keys %pkgs;
+  }
+  return %index;
+}
+
+sub write_repo_index {
+  my ($self) = @_;
+  my %index = $self->build_repo_index;
+  YAML::DumpFile( $self->module_index_file, \%index );
 }
 
 1;
--- t/build-index.t
+++ t/build-index.t
@@ -2,13 +2,32 @@ use warnings;
 use strict;
 use FindBin;
 use Test::More;
+use YAML;
 
 use CPAN::Mirror::FromGit;
 
-my $cmgit = CPAN::Mirror::FromGit->new;
+# bootstrap test with our own repo
+my $repo = "file:///${FindBin::Bin}/../.git";
 
-my %modules = $cmgit->scan_repo("file:///${FindBin::Bin}/../.git");
+my $cmgit = CPAN::Mirror::FromGit->new_with_config(
+  configfile => "$FindBin::Bin/testconfig.yaml",
+  repos      => [$repo],
+  home_dir   => $FindBin::Bin
+);
+my %modules = $cmgit->scan_repo($repo);
 
 is_deeply( \%modules, { 'CPAN::Mirror::FromGit' => undef } );
+
+my %index = $cmgit->build_repo_index;
+
+is_deeply( \%index,
+  { 'CPAN::Mirror::FromGit' => "file:///${FindBin::Bin}/../.git" } );
+
+$cmgit->write_repo_index;
+
+is_deeply( YAML::LoadFile( $cmgit->module_index_file ),
+  { 'CPAN::Mirror::FromGit' => "file:///${FindBin::Bin}/../.git" } );
+
+unlink $cmgit->module_index_file;
 
 done_testing();
